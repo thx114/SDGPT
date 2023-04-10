9902560b4931494ad0a0a0e203d678beeebe3bde
F_thx <1764362678@qq.com>
2023-04-10 22:38:17 +0800

    qaq

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="nonebot-plugin-SDGPT",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="0.1.3",  # 程序版本
+    author="F_thx",  # 项目作者
+    author_email="thx1140093097@gmail.com",  # 作者邮件
+    url="https://github.com/thx114/SDGPT",  # 项目地址
+    packages=setuptools.find_packages(),  # 无需修改
+    license="MIT Licence",
+    install_requires=[
+        'nonebot2',
+        'python-dotenv',
+        'revChatGPT',
+        'EdgeGPT',
+        'webuiapi',
+        'nonebot-adapter-onebot',
+        'nonebot-plugin-guild-patch'
+    ],
+)
+
